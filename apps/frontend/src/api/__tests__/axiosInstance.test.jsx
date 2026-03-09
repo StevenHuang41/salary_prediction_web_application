21120df3d08f4953eabc4c932f86499a91a93080
Steven <yenjung178741@gmail.com>
2026-03-10 01:23:54 +0800

    update gcloud run setting

--- apps/frontend/src/api/__tests__/axiosInstance.test.jsx
+++ apps/frontend/src/api/__tests__/axiosInstance.test.jsx
@@ -6,9 +6,9 @@ beforeEach(() => {
 
 describe('axiosInstance', () => {
   it('create api0 with correct baseURL', async () => {
-    import.meta.env.VITE_API_BASE_URL = 'http://localhost0:8000';
+    import.meta.env.VITE_API_BASE_URL = 'http://localhost0:8080';
 
     const { api0 } = await import("../axiosInstance");
-    expect(api0.defaults.baseURL).toBe('http://localhost0:8000/api/v1');
+    expect(api0.defaults.baseURL).toBe('http://localhost0:8080/api/v1');
   });
 });
